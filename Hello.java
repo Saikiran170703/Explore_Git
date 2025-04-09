a6140235fdf637766672b4e988e67b9d54d75739
saikiran <purushothamsaikiran04@gmail.com>
2025-04-09 19:44:32 +0545

    contains hello message

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.*;
 class Hello{
    public staric void main(String[] args)
@@ -5,3 +6,14 @@ class Hello{
      System.out.println("Hello World !");
   }
 }
+=======
+import jav.util.*;
+ class Hello{
+    public static void main(String[args])
+ 
+      {
+          System.out.println("Hello Worls!");
+        }
+}     
+
+>>>>>>> contains hello message
