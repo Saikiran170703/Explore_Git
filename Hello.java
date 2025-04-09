c3ffb25d34e0b2554928aa83e52c20f912f12e48
Saikiran Purushotham <165788649+Saikiran170703@users.noreply.github.com>
2025-04-09 22:32:06 +0545

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-import java.util.*;
-class Hello{
-   public staric void main(String[] args)
+
+ class Hello{
+   public static void main(String[] args)
   {
      System.out.println("Hello World !");
   }
 }
-=======
-import jav.util.*;
- class Hello{
-    public static void main(String[args])
- 
-      {
-          System.out.println("Hello Worls!");
-        }
-}     
 
->>>>>>> contains hello message
